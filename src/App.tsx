71f323ec185ecaf67946ee90bff14f3e17c8c757
Wildan Najah Wildiansyah <wildan.najah@gmail.com>
2022-04-12 12:34:01 +0700

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import { createTheme, ThemeProvider } from '@mui/material';
-import React from 'react'
+import { createTheme, ThemeOptions, ThemeProvider } from '@mui/material';
+// import React{ useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from './redux/store'
 import Coba from './component/Coba';
-import getTheme from './theme/theme';
+import typography from './theme/typography';
+import breakpoints from './theme/breakpoints';
+import shadows, { customShadows } from './theme/shadows';
+import palette from './theme/palette';
+// import getTheme from './theme/theme';
 
 function App() {
   const { mode } = useSelector((state)=>state.theme);
-  const theme = React.useMemo(() => createTheme(getTheme(mode)), [mode]);
-  
+  const isLight = mode === 'light';
+  const themeOptions: ThemeOptions = useMemo(
+    () => ({
+      palette: isLight ? palette.light : palette.dark,
+      typography,
+      breakpoints,
+      shape: { borderRadius: 8 },
+      shadows: isLight ? shadows.light : shadows.dark,
+      customShadows: isLight ? customShadows.light : customShadows.dark,
+    }),
+    [isLight]
+  );
+  const theme = createTheme(themeOptions);
   return (
     <ThemeProvider theme={theme}>
       <Coba />
